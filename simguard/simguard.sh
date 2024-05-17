9f8b95a006d5016fd3262adb201563cf4bc5060d
Jozsef Barta <>
2024-05-17 11:38:01 +0200

    Update simguard to reset the proper simulator when multiple simulators are booted.

--- simguard/simguard.sh
+++ simguard/simguard.sh
@@ -10,24 +10,62 @@ printHelp()
     echo "-n    Send notification when restart is needed"
 }
 
-killSimulatorIfNeeded()
+error() { echo -e "\xF0\x9F\x9A\xA8 \033[0;31m$1\033[0m"; }
+
+sendNotification()
 {
-    xcrun simctl spawn booted launchctl list | grep $1
-    if [ $? -eq 0 ]; then
-        message="Process $1 has stuck, shutdown Simulator..."
-        if [ $2 == "true" ]; then
-            osascript \
-                -e "on run argv" \
-                -e "display notification item 1 of argv with title \"SimGuard\"" \
-                -e "end run" \
-                -- "$message"
-        fi
-        echo $message
-        xcrun simctl shutdown booted
+    osascript \
+        -e "on run argv" \
+        -e "display notification item 1 of argv with title \"SimGuard\"" \
+        -e "end run" \
+        -- "$1"
+}
+
+killSimualtor()
+{
+    xcrun simctl shutdown $1
+}
+
+listBootedSimulators()
+{
+    xcrun simctl list devices booted | grep -E "[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}" -o
+}
+
+findPid() {
+    xcrun simctl spawn $1 launchctl list | grep $2 | while read LINE ; do
+        set -- $LINE
+        echo $1
+        exit 0
+    done
+}
+
+isProcessStuck()
+{
+    STATE=($(ps -p "$1" -o state=))
+    if [[ $STATE -eq "Z" ]]; then
+        return 0
+    else
+        return 1
     fi
 }
 
-error() { echo -e "\xF0\x9F\x9A\xA8 \033[0;31m$1\033[0m"; }
+shutdownSimulatorIfNeeded() {
+    listBootedSimulators | while read SIM ; do
+        PID=$(findPid $SIM $1)
+        if [[ -nz $PID ]]; then
+            if isProcessStuck $PID; then
+                echo "Process $1 has stuck on $SIM, reset simulator..."
+                if [ $2 == "true" ]; then
+                    SIM_NAME=$(xcrun simctl list devices $SIM | awk '{n=split($0,a,"("); if (n>1) { gsub(/^[ \t]+/, "", a[1]); gsub(/[ \t]+$/, "", a[1]); print a[1]}}')
+                    MESSAGE="Reset simulator $SIM_NAME..."
+                    sendNotification "$MESSAGE"
+                fi
+                killSimualtor $SIM
+                echo $PID
+            fi
+        fi
+    done
+}
 
 BUNDLE_ID=""
 SEND_NOTIFICATION="false"
@@ -64,16 +102,18 @@ else
     echo "Sending notification if process has stuck."
 fi
 
+shutdownSimulatorIfNeeded $BUNDLE_ID $SEND_NOTIFICATION
+
 # First line is the command itself
-FIRSTLINE="true"
+FIRSTLINE=1
 log stream --debug --predicate 'process == "debugserver" and eventMessage contains "Exiting"' | while read line ; do
-    if [ $FIRSTLINE == "true" ]; then
-        FIRSTLINE="false"
+    if [ $FIRSTLINE == 1 ]; then
+        FIRSTLINE=0
         continue
     fi
 
     if [[ $line =~ .*debugserver.*Exiting ]]; then
-        killSimulatorIfNeeded $BUNDLE_ID $SEND_NOTIFICATION
+        shutdownSimulatorIfNeeded $BUNDLE_ID $SEND_NOTIFICATION
     fi
 done
 
